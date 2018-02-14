60687ab316f030c593a07e89c7baa5a3d4a7f9a4
Rebecca Gemeinhardt <rebecca.geme@gmail.com>
2018-02-13 21:05:51 -0600

    game reset works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,11 @@
 //Arrays for the game
-    var monsterList = ["harpy", "kobold", "dryad", "hobgoblin", "manticore","demilich","chimera","aboleth","kraken", "dracolich","owlbear","cockatrice","pseudodragon","rakshasa","minotaur","hippogriff"];
-    var monster = "";
-    var lettersInMonster = [];
-    var numBlanks = 0;
-    var dashes = [];
-    var wrongLetters = [];
-    var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var monsterList = ["harpy", "kobold", "dryad", "hobgoblin", "manticore", "demilich", "chimera", "aboleth", "kraken", "dracolich", "owlbear", "cockatrice", "pseudodragon", "rakshasa", "minotaur", "hippogriff"];
+var monster = "";
+var lettersInMonster = [];
+var numBlanks = 0;
+var dashes = [];
+var wrongLetters = [];
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 //score keepers
 var winCount = 0;
@@ -16,67 +16,86 @@ var guessesLeft = 5;
 var letterIndex;
 var userGuess;
 
+//this updates the scoreboard when the user first loads the page
+chooseMonster();
+updateScoreboard();
+
 // This function makes the computer choose a random monster
 function chooseMonster() {
+
     monster = monsterList[Math.floor(Math.random() * monsterList.length)];
+    console.log(monster);
     //this seperates the word into letters
     lettersInMonster = monster.split("");
     numBlanks = lettersInMonster.length;
-
-//this pushes the correct number of letters for the chosen monster to the array dashes
-for (var i=0; i<numBlanks; i++){
-    dashes.push("_ ");
+    //this pushes the correct number of letters for the chosen monster to the array dashes
+    for (var i = 0; i < numBlanks; i++) {
+        dashes.push("_ ");
+    }
 }
-
-//this updates the scoreboard when the user first loads the page
-updateScoreboard();
 
 //User input
 // This function is run whenever the user presses a key.
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
     userGuess = String.fromCharCode(event.keyCode).toLowerCase();
     letterIndex = alphabet.indexOf(userGuess);
     //This checks to make sure user pressed a letter, not any other key
-    if (letterIndex === -1){
+    if (letterIndex === -1) {
     }
-    else{
+    else {
         letterPressed();
     }
 
 }
+
 //If the user presses a letter, then this function will run
-function letterPressed(){
-    console.log("letterPressed: "+userGuess);
-    letterIndex = monster.indexOf(userGuess)
-    console.log(letterIndex);
-    if (letterIndex === -1){
-        if (searchLettersGuessed() === -1){
+function letterPressed() {
+    for (i = 0; i < monster.length; i++) {
+        letterIndex = monster.indexOf(userGuess, i);
+        if (letterIndex === -1) {
+        }
+        else {
+            dashes[letterIndex] = userGuess;
+        }
+    }
+    letterIndex = monster.indexOf(userGuess);
+    if (letterIndex === -1) {
+        if (searchLettersGuessed() === -1) {
             wrongLetters.push(userGuess);
             guessesLeft--;
         }
-    }   
-
-    else{
-        dashes[letterIndex] = userGuess;
-        console.log('Dashes: '+dashes);
     }
-    
 
-    if(guessesLeft === 0){
+    if (guessesLeft === 0) {
         lossCount++;
         alert("You Lose!");
+        gameOver();
     }
 
+    checkWin();
     //this calls the function to update the scoreboard after each user guess
     updateScoreboard();
-    
-    console.log("Guesses Left: " + guessesLeft)
+}
+
+
+function checkWin() {
+    var lengthCount = 0;
+    for (i = 0; i < dashes.length; i++) {
+        if (dashes[i] === lettersInMonster[i]) {
+            lengthCount++;
+        }
+    }
+    if (lengthCount === dashes.length){
+        winCount++;
+        alert("You Win!");
+        gameOver();
+    }
 }
 
 //this checks to see if the wrong guesses have already been guessed
 function searchLettersGuessed() {
-    for (i=0; i<wrongLetters.length;i++){
-        if (getUserGuess() === wrongLetters[i]){
+    for (i = 0; i < wrongLetters.length; i++) {
+        if (getUserGuess() === wrongLetters[i]) {
             return 1;
         }
     }
@@ -89,7 +108,6 @@ function updateScoreboard() {
     document.getElementById("guessesLeft").innerHTML = guessesLeft;
     document.getElementById("winCounter").innerHTML = winCount;
     document.getElementById("lossCounter").innerHTML = lossCount;
-    //this prints the array to the scoreboard
     document.getElementById("guessesWrong").innerHTML = wrongLetters;
 }
 
@@ -97,19 +115,10 @@ function getUserGuess() {
     return userGuess;
 }
 
-//prints to console for testing
-console.log(monster);
-console.log(lettersInMonster);
-console.log(numBlanks);
-console.log(dashes);
+function gameOver() {
+    wrongLetters = [];
+    guessesLeft = 5;
+    dashes = [];
+    chooseMonster();
+
 }
-
-chooseMonster();
-
-//If user guess is equal to a character within the chosen monster then display user guess
-
-//scoring stuff
-if(guessesLeft === 0){
-        alert("you lose!")
-    }
-    console.log(guessesLeft)
