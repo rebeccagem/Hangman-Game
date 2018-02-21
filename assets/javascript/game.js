ab0e0bf5068424920295faac88106c330f30c1bd
Rebecca Gemeinhardt <rebecca.geme@gmail.com>
2018-02-20 22:57:56 -0600

    final touches!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,19 @@
 //Arrays for the game
-var monsterList = ["aboleth", "chimera", "cockatrice", "demilich", "dracolich", "dryad", "harpy", "hippogriff", "hobgoblin", "kobold", "kraken", "manitcore", "minotaur", "owlbear", "banderhobb", "rakshasa"];
+var monsterList = ["aboleth", "chimera", "cockatrice", "demilich", "dracolich", "dryad", "hippogriff", "kobold",  "manitcore", "minotaur", "owlbear", "banderhobb"];
 var monster = "";
 var lettersInMonster = [];
 var numBlanks = 0;
 var dashes = [];
 var wrongLetters = [];
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var imageArray = ["./assets/images/aboleth.png","./assets/images/chimera.png","./assets/images/cockatrice.png", "./assets/images/demilich.png","./assets/images/dracolich.png","./assets/images/dryad.png", "./assets/images/Placeholder-Harpy.jpg", "./assets/images/hippogriff.png", "./assets/images/Placeholder-Hobgoblin.jpg", "./assets/images/Placeholder-Kobold.jpg", "./assets/images/Placeholder-Kraken.jpg", "./assets/images/manticore.png", "./assets/images/minotaur.png", "./assets/images/Placeholder-Owlbear.jpg", "./assets/images/banderhobb.png", "./assets/images/Placeholder-Rakshasa.jpg"];
+var imageArray = ["./assets/images/aboleth.png", "./assets/images/chimera.png", "./assets/images/cockatrice.png", "./assets/images/demilich.png", "./assets/images/dracolich.png", "./assets/images/dryad.png", "./assets/images/hippogriff.png", "./assets/images/kobold.png", "./assets/images/manticore.png", "./assets/images/minotaur.png", "./assets/images/owlbear.png", "./assets/images/banderhobb.png"];
 
 
 //score keepers
 var winCount = 0;
 var lossCount = 0;
-var guessesLeft = 5;
+var guessesLeft = 5; 
+var firstRun = true;
 
 //holding some variables here for later
 var letterIndex;
@@ -25,9 +26,14 @@ updateScoreboard();
 
 // This function makes the computer choose a random monster
 function chooseMonster() {
-
     randomNum = Math.floor(Math.random() * monsterList.length);
     monster = monsterList[randomNum];
+    console.log(firstRun);
+
+    //if it is the first run Chimera will be the word to guess
+    if (firstRun === true) {
+        monster = monsterList[1];
+    }
     console.log(monster);
     //this seperates the word into letters
     lettersInMonster = monster.split("");
@@ -36,6 +42,13 @@ function chooseMonster() {
     for (var i = 0; i < numBlanks; i++) {
         dashes.push("_ ");
     }
+    //images changing
+    var myImage = imageArray[randomNum];
+    console.log(myImage);
+    if (firstRun === true) {
+        myImage = imageArray[1];
+    }
+    document.getElementById("mainImage").src = myImage;
 }
 
 //User input
@@ -89,20 +102,16 @@ function checkWin() {
             lengthCount++;
         }
     }
-    if (lengthCount === dashes.length){
+    if (lengthCount === dashes.length) {
         winCount++;
-        //images changing
-        var myImage = imageArray[randomNum];
-        console.log(myImage);
-        document.getElementById("mainImage").src = myImage;
 
 
         setTimeout(function () {
-            
+
             alert("You Win!");
-            }, 200);
-            window.setTimeout(gameOver, 400);
-            
+        }, 200);
+        window.setTimeout(gameOver, 400);
+
     }
 }
 
@@ -132,11 +141,13 @@ function getUserGuess() {
 function gameOver() {
     wrongLetters = [];
     guessesLeft = 5;
-    dashes = [];    
+    dashes = [];
+    firstRun = false;
+    console.log(firstRun);
     chooseMonster();
     updateScoreboard();
 
 }
- 
+
 
 
